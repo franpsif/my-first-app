6761211d7c4595d1b4b709f8b930b0f2c639c77b
Francisco Porcel Sendrós <francisco.porcel@unit4.com>
2017-04-03 12:55:05 +0200

    done two exercises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlert } from './warningalert/warningalert.component';
+import { SuccessAlert } from './successalert/successalert.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlert
+    WarningAlert,
+    SuccessAlert
   ],
   imports: [
     BrowserModule,
--- src/app/warningalert/warningalert.component.ts
+++ src/app/warningalert/warningalert.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-warningalert',
-    templateUrl: '../warningalert.component.html'    
+    templateUrl: './warningalert.component.html',
+    styleUrls: ['./warningalert.component.css']
 })
 
 export class WarningAlert {
